0f535a41e5daef4579cfdad83d7aeaf253a6ccd5
Taruchit Goyal <taruchit.goyal@gmail.com>
2021-11-07 23:52:05 +0530

    Adding the print statements

--- NumpyArray.py
+++ NumpyArray.py
@@ -11,10 +11,12 @@ import numpy as np
 numpy_array=np.array([1,2,3,4,5])
 
 #Printing the Numpy array
+print('Printing the array')
 print(numpy_array)
 
 #Broadcast operation
 broadcast_array=numpy_array*2
+print('Broadcast array: -')
 print(broadcast_array)
 
 #Creating a matrix
@@ -23,13 +25,23 @@ matrix=[[1,2,3],
         [7,8,9]]
 
 matrix_numpy_array=np.array(matrix)
+print("Numpy matrix: -")
 print(matrix_numpy_array)
 
 '''Accessing an element from Matrix Numpy array
 Row 0, column 2 -> 3
 '''
+print("x=0,y=2: -")
 print(matrix_numpy_array[0][2])
 
 #Matrix filled by random integers
 matrix_numpy_randomInt=np.random.randint(1,10,(4,4))
+print("Numpy matrix with random integers: -")
 print(matrix_numpy_randomInt)
+
+#Fixing the random seed
+np.random.seed(0)
+matrix_numpy_randomInt_fixSeed=np.random.randint(1,10,(4,4))
+print("Numpy matrix with fixed random integers: -")
+print(matrix_numpy_randomInt_fixSeed)
+
