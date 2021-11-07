684c4e05f0a6cc53bc01954d9a9fb4d23b299668
Taruchit Goyal <taruchit.goyal@gmail.com>
2021-11-07 23:44:28 +0530

    Numpy
    Basic Numpy array operations

---
+++ NumpyArray.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  7 23:27:34 2021
+
+@author: pc
+"""
+#Impporting Numpy library
+import numpy as np
+
+#Creating Numpy array
+numpy_array=np.array([1,2,3,4,5])
+
+#Printing the Numpy array
+print(numpy_array)
+
+#Broadcast operation
+broadcast_array=numpy_array*2
+print(broadcast_array)
+
+#Creating a matrix
+matrix=[[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+matrix_numpy_array=np.array(matrix)
+print(matrix_numpy_array)
+
+'''Accessing an element from Matrix Numpy array
+Row 0, column 2 -> 3
+'''
+print(matrix_numpy_array[0][2])
+
