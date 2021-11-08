1c50895d5da4ce7d4c3c5cba19509d0466b9fc26
Taruchit Goyal <taruchit.goyal@gmail.com>
2021-11-08 14:43:31 +0530

    Calculating combination

--- SciPy.py
+++ SciPy.py
@@ -16,4 +16,14 @@ def quadratic_equation_function(x):
     return x**2+x+1
 
 #Calculating the first derivative of the function at x=2
+print("Calculating derivation of equation with x=2")
 print(derivative(func=quadratic_equation_function,x0=2))
+
+#Importing comb from scipy
+from scipy.special import comb
+
+'''Calculating total number of combinations from 4 different values taken 2 at a time
+4C2'''
+com=comb(4,2)
+print("Combination: 4C2: -")
+print(com)
